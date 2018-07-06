b5a1821f4972b89ad84327497b623554d877ee55
Mykhailo Levchun <mike.levchun@gmail.com>
2018-07-06 12:57:23 +0300

    My Commit

---
+++ LevchunHW2/src/main/java/com/epam/lab/hometask2/Task5.java
@@ -0,0 +1,40 @@
+package com.epam.lab.hometask2;
+
+import java.lang.reflect.Field;
+
+import org.apache.log4j.Logger;
+
+public class Task5 {
+
+	final static Logger LOG = Logger.getLogger(Task5.class);
+	public static void start()  {
+		try{
+		Class<?> cls = Class.forName("com.epam.lab.hometask2.Task5");
+		Object instance = cls.newInstance();
+		set(instance, "salary", 15);
+		set(instance, "firstname", "John");
+		LOG.info("Value is set");
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+
+	public static boolean set(Object object, String fieldName, Object fieldValue) {
+		Class<?> cls = object.getClass();
+		while (cls != null) {
+			try {
+				Field field = cls.getDeclaredField(fieldName);
+				field.setAccessible(true);
+				field.set(object, fieldValue);
+				return true;
+			} catch (NoSuchFieldException e) {
+	            cls = cls.getSuperclass();
+	        } catch (Exception e) {
+	            throw new IllegalStateException(e);
+	        }
+		}
+		return false;
+	}
+
+}
